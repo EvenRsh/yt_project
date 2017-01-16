fa6bb9ea84bc2768c07577c4a9b25477b3f18bc9
EvenRH <15813307192@139.com>
2017-01-17 00:45:56 +0800

    注册表单验证

--- src/js/register.js
+++ src/js/register.js
@@ -6,17 +6,18 @@
 //页面加载
 
 $(function() {
-    // console.log($('form'));
+    var $form = $('form');
+    var btnSubmit = $('#submitButton');
     var $phone = $('#cellPhone');
     var $code = $('#setCode');
     var $password = $('#password');
     var $repassword = $('#repassword');
    
     //手机号码输入判断是否存在
-    $('form').validate();
     var index ;
-    $('form').on('input', ':input#cellPhone', function() {
-        if ($phone.val().length == 11 ) {
+    $form.on('input', ':input#cellPhone', function() {
+        var len = $phone.val().length;
+        if (len == 11 && /^1[3-578]\d{9}$/.test($phone.val())) {
             index=$phone.val();
             //检测数据库是否存在该手机
             $.post('../php/register.php', {
@@ -25,39 +26,25 @@ $(function() {
             }, function(response) {
                 var data = eval('(' + response + ')');
                 if (data.state) {
-
                     console.log(data.message);
                     //验证码按钮点击生成随机验证码
-                    $("#getCode").on('click', function() {
-                        function randomNum() {
-                            var num = '';
-                            var str = '0123456789';
-                            for (var i = 0; i < 6; i++) {
-                                num += parseInt(Math.random() * str.length);
-                            }
-                            return num;
-                        }
+                    $form.on('click',':input#getCode',function() {
                         $('#setCode').val(randomNum());
-                    });
+                    })
                     //判断所有的信息是否已经填写完成
-                    $('form').on('input', function() {
-                        
-                        if ($phone.val().length == 11) {
-                            if ($code.val() && $password.val() && $repassword.val()) {
-                                //提交按钮高亮
-                                $('#submitButton').addClass('active')
-                            }
+                    .on('input', function() {
+                        if ($code.val() && $password.val() && $repassword.val()) {
+                            //提交按钮高亮
+                            btnSubmit.addClass('active')
+                        }else{
+                            btnSubmit.removeClass('active')
                         }
-                    });
+                    })
                     //后提交数据到服务器再保存到数据库
-                    //判断提交按钮是否高亮
-                    $('#submitButton').on('click', function() {
-                        console.log("点击")
-                        
-                        if ($('#submitButton').hasClass('active')) {
-                           
+                    .on('click',':input#submitButton',function() {
+                        //判断提交按钮是否高亮
+                        if (btnSubmit.hasClass('active')) {
                             if (/^\w{6,12}$/.test($password.val())) {
-
                                 if ($password.val() == $repassword.val()) {
                                     // console.log('可以提交数据');
                                     $.post('../php/register.php', {
@@ -82,14 +69,25 @@ $(function() {
 
                     });
                 } else {
-                    console.log(data.message);
+                    alert(data.message);
                 }
             });
-        }else if($phone.val().length > 11){
+        }else if(len > 11){
             $phone.val(index);
+        }else if(!/^1[3-578]\d{9}$/.test($phone.val()) && len == 11){
+            $('<span/>').text('格式不对').insertAfter($phone);
+        }else if(len < 11){
+            $phone.siblings().remove();
         }
-    });
-
+    })
+    function randomNum() {
+        var num = '';
+        var str = '0123456789';
+        for (var i = 0; i < 6; i++) {
+            num += parseInt(Math.random() * str.length);
+        }
+        return num;
+    }
     //登录跳转
     $('#login').on('click', function() {
         window.location.href = 'login.html';
