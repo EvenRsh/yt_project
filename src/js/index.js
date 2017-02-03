36ec0b57264858e76b349c1a7a5791185d91c556
EvenRH <15813307192@139.com>
2017-02-03 16:52:55 +0800

    主页banner轮播实现

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+/*
+==============
+    ver 1.0.1 create by EvenRH 2017 2 - 3
+==============  
+*/
+//页面加载
+
+$(function() {
+   $banner = $('#banner');
+   //banner
+   $banner.rhcarousel({"imglist":["./img/059c1b4b-94cd-4630-b04c-9a189b3a4285.jpg","./img/88b6dad4-b26a-4c4d-a53d-51c41add7be2.jpg","./img/107269b8-1478-4850-8320-21e6307bd3d1.jpg","./img/8fcf3e58-3adf-48c2-9b3a-e6786291a1b3.jpg","./img/ca11e774-32ab-4149-bac2-da2918f4a592.jpg"],"width":1190,"height":470});
+   //tab
+
+
+});
